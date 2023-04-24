0d973e55b72e0937d17c3da9d1f144dfb93d034f
pjh3752 <wngh7247@naver.com>
2023-04-24 15:35:53 +0900

    Initial commit

---
+++ src/main/java/com/szsapi/SzsApplication.java
@@ -0,0 +1,13 @@
+package com.szsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szsapi/SzsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
